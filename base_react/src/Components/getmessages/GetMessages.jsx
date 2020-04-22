bf030e028d182fb5549b961eb3624605f8f0f81c
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-04-22 20:13:23 +0200

    modifier qui ne fonctionne pas

--- base_react/src/Components/getmessages/GetMessages.jsx
+++ base_react/src/Components/getmessages/GetMessages.jsx
@@ -4,6 +4,7 @@ import './getmessages.css';
 
 const Posts = () => {
     const [posts, setPosts] = useState([])
+    const [isShow, setIsShow] = useState(false)
 
     const url = 'http://localhost:5000/postmessages';
 
@@ -19,7 +20,6 @@ const Posts = () => {
     },[])
 
     const deleteCard = (id) => {
-        console.log(id);
         axios.delete(url+'/'+id)
         .then(res => {
             console.log(res.data)
@@ -29,15 +29,20 @@ const Posts = () => {
         })
     }
 
+    const updateCard = () => {
+        setIsShow(!isShow);
+    }
+
+
     return (
         <div>
             <ul className="cards">
                 {posts.map(post => 
                     <div className="card" key={post._id}>
-                        <p className="card__title">{post.title}</p>
-                        <p className="card__message">{post.message}</p>
+                        {isShow ? <input type="text" placeholder={post.title}></input> : <p className="card__title">{post.title}</p>}
+                        {isShow ? <input type="text" placeholder={post.message}></input> : <p className="card__message">{post.message}</p>}
                         <div className="card__buttons">
-                            <button type="submit">Modifier</button>
+                            <button type="submit" onClick={() => { updateCard(post._id)}}>Modifier</button> 
                             <button type="submit" onClick={() => { deleteCard(post._id)}}>Supprimer</button> 
                         </div>  
                     </div>
