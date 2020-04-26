aabe0f3c3d1b9ca9c34a9864c5aa5874eb0b4ae4
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-04-26 20:46:34 +0200

    email et password regex + longueur pseudo

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,6 +1,9 @@
 const express = require('express');
 var router = express.Router();
 
+// Constants
+const EMAIL_REGEX     = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX  = /^(?=.*\d).{4,8}$/;
 
 var {User} = require('../models/User');
 
@@ -23,7 +26,17 @@ router.post('/', (req, res) =>{
         return res.status(400).json({ 'error': 'missing parameters' });
     }
 
-    
+    if (newRecord.pseudo.length >= 13 || newRecord.pseudo.length <= 4) {
+        return res.status(400).json({ 'error': 'wrong username (must be length 5 - 12)' });
+    }
+  
+    if (!EMAIL_REGEX.test(newRecord.mail)) {
+        return res.status(400).json({ 'error': 'email is not valid' });
+    }
+  
+    if (!PASSWORD_REGEX.test(newRecord.password)) {
+        return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
+    }
 
     newRecord.save((err, docs) => {
         if(!err) res.send(docs)
