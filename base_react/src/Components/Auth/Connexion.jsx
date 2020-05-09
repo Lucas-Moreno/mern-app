95b12c5281de29c0f926d5f6b7d42b4b398babfe
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-05-09 22:05:55 +0200

    connexion partiel

--- base_react/src/Components/Auth/Connexion.jsx
+++ base_react/src/Components/Auth/Connexion.jsx
@@ -1,15 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
 const Connexion = () => {
+  const url = "http://localhost:5000/users/login";
+
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirection, setRedirection] = useState(false);
+
+  const connectUser = () => {
+    axios
+      .post(url, {
+        mail,
+        password
+      })
+      .then(() => setRedirection(true));
+  };
+
   return (
     <div>
       <h1>Connexion :</h1>
-      E-mail :<input />
-      Mot de passe :<input />
-      <Link to="/app">
-        <button>Se connecter</button>
-      </Link>
+      E-mail :
+      <input onChange={e => setMail(e.target.value)} type="text" name="mail" />
+      Mot de passe :
+      <input
+        onChange={e => setPassword(e.target.value)}
+        type="text"
+        name="password"
+      />
+      {redirection === true ? <Redirect to="/app" /> : null}
+      <button
+        onClick={() => {
+          connectUser();
+        }}
+        type="submit"
+      >
+        Se connecter
+      </button>
     </div>
   );
 };
