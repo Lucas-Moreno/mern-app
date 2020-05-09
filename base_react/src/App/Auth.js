9d6162975189ec1fb73ba51371d27f4cd40b989f
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-05-09 19:04:26 +0200

    mise en place de la page authentification avec toggle inscription/connexion

--- base_react/src/App/Auth.js
+++ base_react/src/App/Auth.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Connexion from "../Components/Auth/Connexion";
+import Inscription from "../Components/Auth/Inscription";
+import "./auth.css";
 
 const Auth = () => {
+  const [isShow, setIsShow] = useState(false);
+
+  const toggleButton = () => {
+    setIsShow(!isShow);
+  };
+
   return (
     <div>
-      <Connexion />
+      {isShow ? <Inscription /> : <Connexion />}
+      {isShow ? (
+        <p onClick={toggleButton} className="swap_auth">
+          Se connecter
+        </p>
+      ) : (
+        <p onClick={toggleButton} className="swap_auth">
+          S'inscrire
+        </p>
+      )}
     </div>
   );
 };
