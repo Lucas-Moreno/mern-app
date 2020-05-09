b748ee752247e59164c0226cc480fcbd80ff5ccc
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-05-09 19:52:11 +0200

    requête post sur inscription ok front

--- base_react/src/Components/Auth/Inscription.jsx
+++ base_react/src/Components/Auth/Inscription.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Inscription = () => {
+  const url = "http://localhost:5000/users/register";
+
+  const [pseudo, setPseudo] = useState("");
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sendUser = () => {
+    axios.post(url, {
+      pseudo,
+      mail,
+      password
+    });
+  };
   return (
     <div>
       <h1>Inscription :</h1>
-      Pseudo :<input />
-      E-mail :<input />
-      Mot de passe :<input />
-      <button>S'inscrire</button>
+      Pseudo :
+      <input
+        onChange={e => setPseudo(e.target.value)}
+        type="text"
+        name="pseudo"
+      />
+      E-mail :
+      <input onChange={e => setMail(e.target.value)} type="text" name="mail" />
+      Mot de passe :
+      <input
+        onChange={e => setPassword(e.target.value)}
+        type="text"
+        name="password"
+      />
+      <button
+        onClick={() => {
+          sendUser();
+        }}
+        type="submit"
+      >
+        S'inscrire
+      </button>
     </div>
   );
 };
